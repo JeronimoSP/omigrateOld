159be3d30b44563c83103f00bc16202ca6ea6ef9
Your Name <you@example.com>
2023-07-14 16:16:34 +0200

    Changed the username and database for the source and target systems in rpcconnect.py.y
    # Aider chat conversation:

--- rpcconnect.py
+++ rpcconnect.py
@@ -2,14 +2,14 @@
 source_system = {
     'url': 'https://arrow-head.qubiq.es',
     'database': 'arrow-head',
-    'username': 'mfueyo@zmcapitalcorp.com',
+    'username': 'mfueyo@zmcapitalcorp.com'
     'password': '4rr0W-Qub1Q'
 }
 
 # Target system RPC connection details
 target_system = {
     'url': 'https://arrow16e.falkia.es',
-    'database': 'VERDEPRO',
+    'database': 'od0DD9DF',
     'username': 'admin',
     'password': 'admin123'
 }
