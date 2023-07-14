acdb195be44cf1a21e2345cc408180f29c306ee9
Your Name <you@example.com>
2023-07-14 16:14:42 +0200

    Changed the RPC connection details for the source and target systems.y
    # Aider chat conversation:

--- rpcconnect.py
+++ rpcconnect.py
@@ -1,15 +1,15 @@
 # Source system RPC connection details
 source_system = {
-    'url': 'https://demo6.odoo.com/',
-    'database': 'demo_saas-163_5fa3838da1c0_1689331581',
-    'username': 'admin',
-    'password': 'admin'
+    'url': 'https://arrow-head.qubiq.es',
+    'database': 'arrow-head',
+    'username': 'mfueyo@zmcapitalcorp.com',
+    'password': '4rr0W-Qub1Q'
 }
 
 # Target system RPC connection details
 target_system = {
-    'url': 'https://demo6.odoo.com/',
-    'database': 'demo_saas-163_5fa3838da1c0_1689331581',
+    'url': 'https://arrow16e.falkia.es',
+    'database': 'VERDEPRO',
     'username': 'admin',
-    'password': 'admin'
+    'password': 'admin123'
 }
