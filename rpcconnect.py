4a9d276d950ba22639bdd7881834e4011bbf8d12
Your Name <you@example.com>
2023-07-14 12:41:58 +0200

    aider: Added source and target system RPC connection details in `rpcconnect.py`.
    # Aider chat conversation:
    
    # Context:
    USER: hello
    USER: Do you know Odoo
    USER: I need to upgrade from odoo 15 to odoo 16 and move all accounting data from one system to the other
    USER: We are going to use a different approach, we are going to transfer data from odoo 15 to odoo 16 using rpc connections
    USER: I added these *read-write* files: rpcconnect.py
    USER: in `rpcconnect.py`
    USER: in `rpcconnect.py` add two dictionarys one for source system and the other for target system to handle Odoo rpc connection including but not limmitted to; url, database, user, password

---
+++ rpcconnect.py
@@ -0,0 +1,15 @@
+# Source system RPC connection details
+source_system = {
+    'url': 'http://source_odoo_url',
+    'database': 'source_database_name',
+    'username': 'source_username',
+    'password': 'source_password'
+}
+
+# Target system RPC connection details
+target_system = {
+    'url': 'http://target_odoo_url',
+    'database': 'target_database_name',
+    'username': 'target_username',
+    'password': 'target_password'
+}
