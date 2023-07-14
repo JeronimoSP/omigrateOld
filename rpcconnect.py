d81e1527023ebf15495192ed77e6281e38768025
Your Name <you@example.com>
2023-07-14 12:50:57 +0200

    Changed the source and target system RPC connection details to use a different URL, database, username, and password.y
    # Aider chat conversation:

--- rpcconnect.py
+++ rpcconnect.py
@@ -1,15 +1,15 @@
 # Source system RPC connection details
 source_system = {
-    'url': 'http://source_odoo_url',
-    'database': 'source_database_name',
-    'username': 'source_username',
-    'password': 'source_password'
+    'url': 'https://demo6.odoo.com/',
+    'database': 'demo_saas-163_5fa3838da1c0_1689331581',
+    'username': 'admin',
+    'password': 'admin'
 }
 
 # Target system RPC connection details
 target_system = {
-    'url': 'http://target_odoo_url',
-    'database': 'target_database_name',
-    'username': 'target_username',
-    'password': 'target_password'
+    'url': 'https://demo6.odoo.com/',
+    'database': 'demo_saas-163_5fa3838da1c0_1689331581',
+    'username': 'admin',
+    'password': 'admin'
 }
